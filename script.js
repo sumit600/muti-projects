ac72c12474bcf6141300ab5bc1d0a35b0990f4ab
Sumit Kumar Sharma <shivakumar555zxc@gmail.com>
2025-12-27 19:09:04 +0530

    Added back and todo page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function openFeature(){
+    
 let elem = document.querySelectorAll('.elem')
 
 elem.forEach(function(elem){
@@ -6,4 +7,16 @@ elem.forEach(function(elem){
         document.querySelectorAll(".fullPage")[elem.id].style.display = "block"
     })
 })
+
+let backbtn = document.querySelectorAll(".back")
+
+backbtn.forEach(function(elem){
+   elem.addEventListener('click',function(){
+    document.querySelectorAll(".fullPage")[elem.id].style.display = "none"
+
+   })
+})
+}
+
+openFeature()
 
