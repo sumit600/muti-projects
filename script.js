98bbad56293118cf7d1eac33121a3d2be459919a
Sumit Kumar Sharma <shivakumar555zxc@gmail.com>
2025-12-29 22:50:08 +0530

    Added complete todo-page

--- script.js
+++ script.js
@@ -25,31 +25,43 @@ let input = document.querySelector(".todo-form input")
 let checkbox = document.querySelector(".todo-imp input")
 let textarea = document.querySelector(".todo-form textarea")
 let btn = document.querySelector("#todo-btn")
-let tasklist = [
-    {task:'do push ups',
-    desc:'3 set 1st set 30reps 2nd set 15rep 3rd set 10rep',
-    important:'true'},
-    {task:'start learning',
-    desc:'web development',
-    important:'true'},
-]
+
+// localStorage.clear()
+let tasklist = []
+    tasklist = JSON.parse(localStorage.getItem("tasklist")) || []
+
+
 
 function rendertask(){
     let alltask = document.querySelector('.todo-right')
+    
+    
+    
 
 var sum = ''
-tasklist.forEach((elem)=>{
+tasklist.forEach((elem,idx)=>{
     sum +=  `<div class="right-card">
                     <div class="class-top"> 
                     <h2>${elem.task}</h2>
                     <p>imp</p> 
                     </div>
                     <h3>${elem.desc}</h3>
-                    <button>Completed</button>
+                    <button id="${idx}">Completed</button>
                 </div>`
 })
 
 alltask.innerHTML = sum
+const tasklistString = JSON.stringify(tasklist);
+    localStorage.setItem('tasklist',tasklistString)
+
+let rightBtn = document.querySelectorAll('.right-card button')
+    rightBtn.forEach((elem)=>{
+        elem.addEventListener('click',function(){
+            tasklist.splice(elem.id,1)
+            rendertask()
+        })
+    })
+    
 }
 
 rendertask()
@@ -57,11 +69,6 @@ rendertask()
 todoForm.addEventListener('submit',function(e){
     e.preventDefault()
     
-
-    console.log()
-    console.log()
-    console.log()
-
     tasklist.push(
         {
         task:input.value,
@@ -69,8 +76,17 @@ todoForm.addEventListener('submit',function(e){
         important:checkbox.checked
         }
     )
+    rendertask()
+    
+    
     input.value =''
     textarea.value = ''
     checkbox.checked  = ''
-    rendertask()
+    
+    
 })
+
+
+
+
+ 
